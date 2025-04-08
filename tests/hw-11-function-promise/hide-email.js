2127d5b3094fe79f9ca4bfcea7a45cb6f8ddb4f4
Nazar Kernytskyi <djkera.nk@gmail.com>
2025-04-08 22:52:35 +0300

    hw11-promise-exercises

---
+++ tests/hw-11-function-promise/hide-email.js
@@ -0,0 +1,6 @@
+/*
+Write a JavaScript function that hides email addresses to prevent unauthorized access.
+Test Data :
+console.log(protect_email("robin_singh@example.com"));
+"robin...@example.com"
+*/

a199b4a31a8872ef8d040bef2db4853fe993f443
Nazar Kernytskyi <djkera.nk@gmail.com>
2025-04-09 13:37:37 +0300

    hw11-promise-exercises

--- tests/hw-11-function-promise/hide-email.js
+++ tests/hw-11-function-promise/hide-email.js
@@ -4,3 +4,12 @@ Test Data :
 console.log(protect_email("robin_singh@example.com"));
 "robin...@example.com"
 */
+
+function protect_email(email) {
+    const [userPart, domainPart] = email.split("@");
+    const half = Math.floor(userPart.length / 2);
+    const visible = userPart.substring(0, half); // видима частина
+    return visible + "..." + "@" + domainPart;
+}
+
+console.log(protect_email("robin_singh@example.com"));

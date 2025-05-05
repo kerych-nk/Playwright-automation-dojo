d89c783c2d41f4f15b4c2fdda6b403a48832fe83
Nazar Kernytskyi <djkera.nk@gmail.com>
2025-05-05 21:59:15 +0300

    api tests for articles

---
+++ tests/hw-18-api/tests/request.fixture.ts
@@ -0,0 +1,32 @@
+import { test as base, expect } from "@playwright/test";
+
+export { expect };
+
+type MyFixture = {
+    authData: { email?: string; password?: string };
+};
+
+export const test = base.extend<MyFixture>({
+    authData: {},
+    request: async ({ playwright, request, authData }, use) => {
+        const authRequestBody = {
+            user: { email: authData.email, password: process.env.PASSWORD! },
+        };
+        console.log(authRequestBody);
+
+        const authResponse = await request.post("/api/users/login", {
+            data: authRequestBody,
+            failOnStatusCode: true,
+        });
+
+        const authResponseJson = await authResponse.json();
+        const token = authResponseJson.user.token;
+
+        const apiContext = await playwright.request.newContext({
+            extraHTTPHeaders: {
+                Authorization: "Token " + token,
+            },
+        });
+        use(apiContext);
+    },
+});
